45f1f193285609c62a2cbf7e688cac218fdcc162
Ahmed-Elshennawy <ahmedlshennawy10@gmail.com>
2025-05-14 16:41:00 +0300

    Add Failure class and UseCase abstraction; implement FetchFeaturedBooks and FetchNewest use cases with error handling

--- lib/features/home/domain/repositories/home_repo.dart
+++ lib/features/home/domain/repositories/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:books/core/errors/failure.dart';
 import 'package:books/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:books/core/errors/failure.dart';
+import 'package:books/core/use_cases/use_case.dart';
+import 'package:books/features/home/domain/entities/book_entity.dart';
+import 'package:books/features/home/domain/repositories/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParams> {
+  final HomeRepo homeRepo;
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParams? params]) async {
+    return await homeRepo.fetchFeaturedBooks();
+  }
+}
