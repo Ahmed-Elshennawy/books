595ca2bbec2afe88e8d229400f2f52898c09395e
Ahmed-Elshennawy <ahmedlshennawy10@gmail.com>
2025-05-14 18:50:34 +0300

    Implement fetchFeaturedBooks method in HomeRemoteDataSourceImpl; add getBooksList helper method for data parsing

--- lib/features/home/data/data_sources/remote_data_source.dart
+++ lib/features/home/data/data_sources/remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:books/core/services/api_service.dart';
+import 'package:books/features/home/data/models/book_model/book_model.dart';
 import 'package:books/features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -6,10 +8,25 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl(this.apiService);
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+      endpoint: '?Filtering=free-ebooks&q=programming',
+    );
+    List<BookEntity> books = getBooksList(data); // Books List
+    return books;
+  }
+
+  // Method to get the books list from the data
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
