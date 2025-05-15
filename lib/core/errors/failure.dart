1a8293181f1d3cc6dc5f94c6874b9a0b19eca1a7
Ahmed-Elshennawy <ahmedlshennawy10@gmail.com>
2025-05-15 11:24:25 +0300

    Implement HomeRepoImpl with fetchFeaturedBooks and fetchNewestBooks methods; handle errors with ServerFailure

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,11 +1,50 @@
-class Failure {
-  // final String message;
-  // final int? code;
+import 'package:dio/dio.dart';
 
-  // Failure({required this.message, this.code});
+abstract class Failure {
+  final String message;
 
-  // @override
-  // String toString() {
-  //   return 'Failure(message: $message, code: $code)';
-  // }
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout With Server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout With Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout With Server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          e.response!.statusCode!,
+          e.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to API server was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No Internet Connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unknown error occurred, please try again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    switch (statusCode) {
+      case 400:
+      case 401:
+      case 403:
+        return ServerFailure(response['error']['message']);
+      case 404:
+        return ServerFailure('Your request not found, please try again later');
+      case 500:
+        return ServerFailure('Internal Server Error, please try again later');
+      default:
+        return ServerFailure('Something went wrong, please try again');
+    }
+  }
 }
---
+++ lib/features/home/data/repositories_impl/home_repo_impl.dart
@@ -0,0 +1,52 @@
+import 'package:books/core/errors/failure.dart';
+import 'package:books/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:books/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:books/features/home/domain/entities/book_entity.dart';
+import 'package:books/features/home/domain/repositories/home_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDataSource.fetchNewestBooks();
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
