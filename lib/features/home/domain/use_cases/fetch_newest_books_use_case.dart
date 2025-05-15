b65c812e923eb5cf25b9b95914dab89f79645d32
Ahmed-Elshennawy <ahmedlshennawy10@gmail.com>
2025-05-15 11:46:16 +0300

    Implement cubits for fetching featured and newest books; add corresponding states for managing loading and error scenarios

--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -4,9 +4,9 @@ import 'package:books/features/home/domain/entities/book_entity.dart';
 import 'package:books/features/home/domain/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchNewestUsecase extends UseCase<List<BookEntity>, NoParams> {
+class FetchNewestBooksUsecase extends UseCase<List<BookEntity>, NoParams> {
   final HomeRepo homeRepo;
-  FetchNewestUsecase(this.homeRepo);
+  FetchNewestBooksUsecase(this.homeRepo);
 
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParams? params]) async {
---
+++ lib/features/home/presentation/blocs/newest_books.dart/newest_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:books/features/home/domain/entities/book_entity.dart';
+import 'package:books/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.newestBooksUsecase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUsecase newestBooksUsecase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    final result = await newestBooksUsecase.call();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.message)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
